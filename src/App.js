514766d8f755cfa3a0216408071f7dc6a3165e97
MartinPucan <pucan.m@seznam.cz>
2020-10-25 18:22:07 +0100

    fetched users to table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
-import React, { useState } from 'react';
-import { DatePicker, message } from 'antd';
+import React, {useEffect, useState} from 'react';
+import Table from 'antd/es/table';
+import Avatar from 'antd/es/avatar';
+import Divider from 'antd/es/divider';
 import 'antd/dist/antd.css';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
-	const [date, setDate] = useState(null);
-	const handleChange = value => {
-		message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
-		setDate(value);
-	};
+
+	const [ users, setUsers ] = useState([]);
+	const apiURL = 'https://reqres.in/api/users?page=2';
+
+	useEffect(() => {
+		axios.get(apiURL)
+			.then( response => {
+				setUsers(response.data.data);
+			})
+			.catch(error => {
+				console.log(error);
+			})
+	}, [])
+
+
+	const columns = [{
+		title: 'Avatar',
+		dataIndex: 'avatar',
+		render: url => <Avatar size="small" src={url} />,
+	}, {
+		title: 'Email',
+		dataIndex: 'email',
+	}, {
+		title: 'First name',
+		dataIndex: 'first_name',
+	}, {
+		title: 'Last name',
+		dataIndex: 'last_name',
+	}, {
+		title: 'Action',
+		key: 'action',
+		render: (text, record) => (
+			<span>
+      <a href="#">Action 一 {record.name}</a>
+      <Divider type="vertical" />
+      <a href="#">Delete</a>
+      <Divider type="vertical" />
+    </span>
+		),
+	}];
+
+	console.log(users);
+
+
 	return (
 		<div className="container">
-			<DatePicker onChange={handleChange} />
-			<div style={{ marginTop: 16 }}>
-				Selected Date: { date ? date.format('YYYY-MM-DD') : 'None' }
-			</div>
+			<Table
+				columns={columns}
+				dataSource={users}
+				title={() => 'Users'}
+				pagination={{ pageSize: 4 }}
+				rowKey="id"
+				bordered />
 		</div>
 	);
 };
